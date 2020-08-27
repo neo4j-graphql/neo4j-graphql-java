48f01695a7c3b78ffcea712eaac90eed05852054
Andreas Berger <andreas@berger-ecommerce.com>
2020-08-27 15:21:55 +0200

    Improve test navigation
    This change adds support for:
    * nesting tests (deep hierarchy)
    * direct navigation to failed block (cypher or cypher params)
    * let augmentation-tests provides a diff on failure that can be viewed in IntelliJ

--- src/test/kotlin/org/neo4j/graphql/utils/GraphQLSchemaTestSuite.kt
+++ src/test/kotlin/org/neo4j/graphql/utils/GraphQLSchemaTestSuite.kt
@@ -11,64 +11,37 @@ import graphql.schema.diff.reporting.CapturingReporter
 import graphql.schema.idl.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
 import org.neo4j.graphql.DynamicProperties
 import org.neo4j.graphql.SchemaBuilder
 import org.neo4j.graphql.SchemaConfig
-import java.io.File
+import org.opentest4j.AssertionFailedError
+import java.util.*
 import java.util.regex.Pattern
-import javax.ws.rs.core.UriBuilder
+import java.util.stream.Stream
 
-class GraphQLSchemaTestSuite(fileName: String) : AsciiDocTestSuite() {
-    val schema: String
-    val schemaPrinter = SchemaPrinter(SchemaPrinter.Options.defaultOptions()
-        .includeDirectives(true)
-        .includeScalarTypes(true)
-        .includeExtendedScalarTypes(true)
-        .includeSchemaDefintion(true)
-        .includeIntrospectionTypes(false)
-        .setComparators(DefaultSchemaPrinterComparatorRegistry.newComparators()
-            .addComparator({ env ->
-                env.parentType(GraphQLObjectType::class.java)
-                env.elementType(GraphQLFieldDefinition::class.java)
-            }, GraphQLFieldDefinition::class.java, fun(o1: GraphQLFieldDefinition, o2: GraphQLFieldDefinition): Int {
-                val (op1, name1) = o1.splitName()
-                val (op2, name2) = o2.splitName()
-                if (op1 == null && op2 == null) {
-                    return name1.compareTo(name2)
-                }
-                if (op1 == null) {
-                    return -1
-                }
-                if (op2 == null) {
-                    return 1
-                }
-                val prio1 = name1.compareTo(name2)
-                if (prio1 == 0) {
-                    return op1.compareTo(op2)
-                }
-                return prio1
-            })
-            .build())
-    )
+class GraphQLSchemaTestSuite(fileName: String) : AsciiDocTestSuite(fileName) {
 
-    class TestRun(
-            private val suite: GraphQLSchemaTestSuite,
-            val title: String?,
-            var file: File,
-            val line: Int,
-            private val config: SchemaConfig,
-            private val targetSchema: String,
-            private val ignore: Boolean) {
+    fun run(): Stream<DynamicNode> {
+        return parse(linkedSetOf("[source,json]", "[source,graphql]"))
+    }
 
-        fun run() {
-            println(title)
+    override fun testFactory(title: String, schema: String, codeBlocks: Map<String, ParsedBlock>, ignore: Boolean): List<DynamicNode> {
+        val targetSchemaBlock = codeBlocks["[source,graphql]"]
+        val compareSchemaTest = DynamicTest.dynamicTest("compare schema", targetSchemaBlock?.uri, {
+            val configBlock = codeBlocks["[source,json]"]
+            val config = MAPPER.readValue(configBlock?.code?.toString()
+                    ?: throw IllegalStateException("missing config $title"), SchemaConfig::class.java)
+
+            val targetSchema = targetSchemaBlock?.code?.trim()?.toString()
+                    ?: throw IllegalStateException("missing graphql for $title")
+
+            var augmentedSchema: GraphQLSchema? = null
+            var expectedSchema: GraphQLSchema? = null
             try {
-                val augmentedSchema = SchemaBuilder.buildSchema(suite.schema, config)
+                augmentedSchema = SchemaBuilder.buildSchema(schema, config)
                 val schemaParser = SchemaParser()
-
-                println("Augmented Schema:")
-                println(suite.schemaPrinter.print(augmentedSchema))
 
                 val reg = schemaParser.parse(targetSchema)
                 val schemaGenerator = SchemaGenerator()
@@ -76,60 +49,61 @@ class GraphQLSchemaTestSuite(fileName: String) : AsciiDocTestSuite() {
                 reg
                     .getTypes(InterfaceTypeDefinition::class.java)
                     .forEach { typeDefinition -> runtimeWiring.type(typeDefinition.name) { it.typeResolver { null } } }
-                val expected = schemaGenerator.makeExecutableSchema(reg, runtimeWiring
+                expectedSchema = schemaGenerator.makeExecutableSchema(reg, runtimeWiring
                     .scalar(DynamicProperties.INSTANCE)
                     .build())
 
-                diff(expected, augmentedSchema)
-                diff(augmentedSchema, expected)
+                diff(expectedSchema, augmentedSchema)
+                diff(augmentedSchema, expectedSchema)
             } catch (e: Throwable) {
                 if (ignore) {
                     Assumptions.assumeFalse(true, e.message)
                 } else {
-                    throw e
+                    throw AssertionFailedError("augmented schema differs for '$title'",
+                            expectedSchema?.let { SCHEMA_PRINTER.print(it) } ?: targetSchema,
+                            SCHEMA_PRINTER.print(augmentedSchema),
+                            e)
+
                 }
             }
-        }
-
-        private fun diff(augmentedSchema: GraphQLSchema, expected: GraphQLSchema) {
-            val diffSet = DiffSet.diffSet(augmentedSchema, expected)
-            val capture = CapturingReporter()
-            SchemaDiff(SchemaDiff.Options.defaultOptions().enforceDirectives())
-                .diffSchema(diffSet, capture)
-            assertThat(capture.dangers).isEmpty()
-            assertThat(capture.breakages).isEmpty()
-        }
-    }
-
-    val tests: List<TestRun>
-
-    init {
-        val result = parse(fileName, linkedSetOf("[source,json]", "[source,graphql]"))
-        schema = result.schema
-        tests = result.tests.map {
-            TestRun(this,
-                    it.title,
-                    result.file,
-                    it.line,
-                    MAPPER.readValue(it.codeBlocks["[source,json]"]?.toString()
-                            ?: throw IllegalStateException("missing config ${it.title}"), SchemaConfig::class.java),
-                    it.codeBlocks["[source,graphql]"]?.trim()?.toString()
-                            ?: throw IllegalStateException("missing graphql for ${it.title}"),
-                    it.ignore
-            )
-        }
-    }
-
-    fun run(): List<DynamicTest> {
-        return tests.map {
-            DynamicTest.dynamicTest(
-                    it.title,
-                    UriBuilder.fromUri(it.file.toURI()).queryParam("line", it.line).build()) { it.run() }
-        }
+        })
+        return Collections.singletonList(compareSchemaTest)
     }
 
     companion object {
         private val METHOD_PATTERN = Pattern.compile("(add|delete|update|merge|create)(.*)")
+
+        private val SCHEMA_PRINTER = SchemaPrinter(SchemaPrinter.Options.defaultOptions()
+            .includeDirectives(true)
+            .includeScalarTypes(true)
+            .includeExtendedScalarTypes(true)
+            .includeSchemaDefintion(true)
+            .includeIntrospectionTypes(false)
+            .setComparators(DefaultSchemaPrinterComparatorRegistry.newComparators()
+                .addComparator({ env ->
+                    env.parentType(GraphQLObjectType::class.java)
+                    env.elementType(GraphQLFieldDefinition::class.java)
+                }, GraphQLFieldDefinition::class.java, fun(o1: GraphQLFieldDefinition, o2: GraphQLFieldDefinition): Int {
+                    val (op1, name1) = o1.splitName()
+                    val (op2, name2) = o2.splitName()
+                    if (op1 == null && op2 == null) {
+                        return name1.compareTo(name2)
+                    }
+                    if (op1 == null) {
+                        return -1
+                    }
+                    if (op2 == null) {
+                        return 1
+                    }
+                    val prio1 = name1.compareTo(name2)
+                    if (prio1 == 0) {
+                        return op1.compareTo(op2)
+                    }
+                    return prio1
+                })
+                .build())
+        )
+
         fun GraphQLType.splitName(): Pair<String?, String> {
             val m = METHOD_PATTERN.matcher(this.name)
             return if (m.find()) {
@@ -137,6 +111,15 @@ class GraphQLSchemaTestSuite(fileName: String) : AsciiDocTestSuite() {
             } else {
                 null to this.name.toLowerCase()
             }
+        }
+
+        fun diff(augmentedSchema: GraphQLSchema, expected: GraphQLSchema) {
+            val diffSet = DiffSet.diffSet(augmentedSchema, expected)
+            val capture = CapturingReporter()
+            SchemaDiff(SchemaDiff.Options.defaultOptions().enforceDirectives())
+                .diffSchema(diffSet, capture)
+            assertThat(capture.dangers).isEmpty()
+            assertThat(capture.breakages).isEmpty()
         }
     }
 }
