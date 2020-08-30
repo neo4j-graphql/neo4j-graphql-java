ef9279f70ce5d187557b3a1cf7bf8841c0fa51eb
Andreas Berger <Andy2003@users.noreply.github.com>
2020-08-31 00:34:38 +0200

    Improve test navigation (#114)
    This change adds support for:
    * nesting tests (deep hierarchy)
    * direct navigation to failed block (cypher or cypher params)
    * let augmentation-tests provides a diff on failure that can be viewed in IntelliJ

--- src/test/kotlin/org/neo4j/graphql/utils/AsciiDocTestSuite.kt
+++ src/test/kotlin/org/neo4j/graphql/utils/AsciiDocTestSuite.kt
@@ -1,74 +1,145 @@
 package org.neo4j.graphql.utils
 
 import org.codehaus.jackson.map.ObjectMapper
+import org.junit.jupiter.api.DynamicContainer
+import org.junit.jupiter.api.DynamicNode
 import java.io.File
+import java.net.URI
+import java.util.regex.Pattern
+import java.util.stream.Stream
+import javax.ws.rs.core.UriBuilder
 
-open class AsciiDocTestSuite {
-    class ParsedFile(
-            var schema: String,
-            var file: File,
-            val tests: MutableList<ParsedBlock>
-    )
+open class AsciiDocTestSuite(private val fileName: String) {
 
     class ParsedBlock(
-            var title: String? = null,
-            var line: Int = 0,
-            var ignore: Boolean = false,
-            val codeBlocks: MutableMap<String, StringBuilder> = mutableMapOf()
+            val uri: URI,
+            val code: StringBuilder
     )
+
+    fun parse(blocks: LinkedHashSet<String>): Stream<DynamicNode> {
+        val file = File(AsciiDocTestSuite::class.java.getResource("/$fileName").toURI())
+        val srcLocation = File("src/test/resources/", fileName).toURI()
+        var root: DocumentLevel? = null
+        var currentDocumentLevel: DocumentLevel? = null
+
+        val lines = file.readLines()
+        val terminatorElement = blocks.lastOrNull()
+
+        var schema: String? = null
+        var title: String? = null
+        var current: StringBuilder? = null
+
+        var codeBlocks = mutableMapOf<String, ParsedBlock>()
+        var ignore = false
+        var inside = false
+
+        var currentDepth = 0
+
+        loop@ for ((lineNr, line) in lines.withIndex()) {
+            if (line.startsWith("#") || line.startsWith("//")) {
+                continue
+            }
+            val headlineMatcher = HEADLINE_PATTERN.matcher(line)
+            when {
+                line == "[source,graphql,schema=true]" -> schema = ""
+                blocks.contains(line) -> {
+                    current = StringBuilder()
+                    codeBlocks[line] = ParsedBlock(
+                            UriBuilder.fromUri(srcLocation).queryParam("line", lineNr + 1).build(),
+                            current
+                    )
+                }
+                line == "----" -> {
+                    if (schema?.isNotBlank() == true && current == null) {
+                        val tests = schemaTestFactory(schema)
+                        currentDocumentLevel?.tests?.add(tests)
+                        if (terminatorElement == null) {
+                            break@loop
+                        }
+                    }
+                    if (codeBlocks[terminatorElement]?.code?.isNotEmpty() == true) {
+                        val tests = testFactory(
+                                title ?: throw IllegalStateException("Title should be defined (line $lineNr)"),
+                                schema ?: throw IllegalStateException("Schema should be defined"),
+                                codeBlocks,
+                                ignore)
+                        currentDocumentLevel?.tests?.add(tests)
+                        codeBlocks = mutableMapOf()
+                    }
+                    inside = !inside
+                }
+                headlineMatcher.matches() -> {
+                    val uri = UriBuilder.fromUri(srcLocation).queryParam("line", lineNr + 1).build()
+                    val depth = headlineMatcher.group(1).length
+                    title = headlineMatcher.group(2)
+                    if (root == null) {
+                        root = DocumentLevel(null, title, uri)
+                        currentDocumentLevel = root
+                    } else {
+                        val parent = when {
+                            depth > currentDepth -> currentDocumentLevel
+                            depth == currentDepth -> currentDocumentLevel?.parent
+                                    ?: throw IllegalStateException("cannot create sub-level on null")
+                            else -> currentDocumentLevel?.parent?.parent
+                                    ?: throw IllegalStateException("cannot create sub-level on null")
+                        }
+                        currentDocumentLevel = DocumentLevel(parent, title, uri)
+                    }
+                    currentDepth = depth
+                }
+                line.startsWith("CAUTION:") -> ignore = true
+                inside -> when {
+                    current != null -> current.append(line).append("\n")
+                    schema != null -> schema += line + "\n"
+                }
+            }
+        }
+
+        return root?.generateTests() ?: Stream.empty()
+    }
+
+    open fun testFactory(title: String, schema: String, codeBlocks: Map<String, ParsedBlock>, ignore: Boolean): List<DynamicNode> {
+        return emptyList()
+    }
+
+    open fun schemaTestFactory(schema: String): List<DynamicNode> {
+        return emptyList()
+    }
 
     companion object {
         val MAPPER = ObjectMapper()
+        val HEADLINE_PATTERN: Pattern = Pattern.compile("^(=+) (.*)$")
 
-        fun parse(fileName: String, blocks: LinkedHashSet<String>): ParsedFile {
-            val file = File(AsciiDocTestSuite::class.java.getResource("/$fileName").toURI())
-            val lines = file.readLines()
-            val terminatorElement = blocks.last()
-            val tests: MutableList<ParsedBlock> = mutableListOf()
+        class DocumentLevel(
+                val parent: DocumentLevel?,
+                val name: String,
+                private val testSourceUri: URI
+        ) {
+            private val children = mutableListOf<DocumentLevel>()
+            val tests = mutableListOf<List<DynamicNode>>()
 
-            var schema: String? = null
-            var lastTitle: String? = null
-            var titleCount = 1
-            var current: StringBuilder? = null
-
-            var testSet = ParsedBlock()
-            var inside = false
-            for ((lineNr, line) in lines.withIndex()) {
-                if (line.startsWith("#") || line.startsWith("//")) {
-                    continue
-                }
-                when {
-                    line == "[source,graphql,schema=true]" -> schema = ""
-                    blocks.contains(line) -> {
-                        current = StringBuilder()
-                        testSet.codeBlocks[line] = current
-                    }
-                    line == "----" -> {
-                        if (testSet.codeBlocks[terminatorElement]?.isNotEmpty() == true) {
-                            tests.add(testSet)
-                            if (testSet.title == null) {
-                                testSet.title = lastTitle + " " + ++titleCount
-                            } else {
-                                titleCount = 1
-                                lastTitle = testSet.title
-                            }
-                            testSet = ParsedBlock()
-                        }
-                        inside = !inside
-                    }
-                    line.startsWith("=== ") -> {
-                        testSet.title = line.substring(4)
-                        testSet.line = lineNr + 1
-                    }
-                    line.startsWith("CAUTION:") -> testSet.ignore = true
-                    inside -> when {
-                        current != null -> current.append(line).append("\n")
-                        schema != null -> schema += line + "\n"
-                    }
-                }
+            init {
+                parent?.children?.add(this)
             }
-            return ParsedFile(schema ?: throw IllegalStateException("no schema found"),
-                    File("src/test/resources/$fileName").absoluteFile, tests)
+
+            fun generateTests(): Stream<DynamicNode> {
+                val streamBuilder = Stream.builder<DynamicNode>()
+                if (tests.size > 1) {
+                    if (children.isNotEmpty()) {
+                        streamBuilder.add(DynamicContainer.dynamicContainer(name, testSourceUri, children.stream().flatMap { it.generateTests() }))
+                    }
+                    for ((index, test) in tests.withIndex()) {
+                        streamBuilder.add(DynamicContainer.dynamicContainer(name + " " + (index + 1), testSourceUri, test.stream()))
+                    }
+                } else {
+                    val nodes = Stream.concat(
+                            tests.stream().flatMap { it.stream() },
+                            children.stream().flatMap { it.generateTests() }
+                    )
+                    streamBuilder.add(DynamicContainer.dynamicContainer(name, testSourceUri, nodes))
+                }
+                return streamBuilder.build()
+            }
         }
 
         private fun fixNumber(v: Any?): Any? = when (v) {
