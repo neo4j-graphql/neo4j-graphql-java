310df5ef4320a6f2ce2af1f84c481c73e930903b
Dirk Mahler <dirk.mahler@asml.com>
2021-02-25 18:29:58 +0100

    added alias or fieldname as variable to Cypher.kt

--- core/src/main/kotlin/org/neo4j/graphql/Cypher.kt
+++ core/src/main/kotlin/org/neo4j/graphql/Cypher.kt
@@ -2,7 +2,7 @@ package org.neo4j.graphql
 
 import graphql.schema.GraphQLType
 
-data class Cypher @JvmOverloads constructor(val query: String, val params: Map<String, Any?> = emptyMap(), var type: GraphQLType? = null) {
+data class Cypher @JvmOverloads constructor(val query: String, val params: Map<String, Any?> = emptyMap(), var type: GraphQLType? = null, var variable: String? = null) {
     fun with(p: Map<String, Any?>) = this.copy(params = this.params + p)
     fun escapedQuery() = query.replace("\"", "\\\"").replace("'", "\\'")
 
