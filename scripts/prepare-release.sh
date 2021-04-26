ae989f315c7710d5b28f899298fb2c1f7040f95f
Andreas Berger <andreas@berger-ecommerce.com>
2021-04-26 11:15:17 +0200

    use profile to update versions in readme.adoc only when preparing the release

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -6,8 +6,8 @@ cd "$root" || exit
 ./mvnw gitflow:release-start
 
 # update readme.adoc
-./mvnw process-resources
-
+./mvnw process-resources -P update-doc
 git add --ignore-errors -A -f -- readme.adoc
 git commit --amend --no-edit
+
 ./mvnw gitflow:release-finish
