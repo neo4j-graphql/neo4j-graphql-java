95a63e0e1b0e1c43ab09246a463fea3de99ede87
Andreas Berger <Andy2003@users.noreply.github.com>
2022-03-29 17:32:57 +0200

    Improve dgs example: use generated constants (#271)

--- examples/dgs-spring-boot/src/main/kotlin/org/neo4j/graphql/examples/dgsspringboot/datafetcher/AdditionalDataFetcher.kt
+++ examples/dgs-spring-boot/src/main/kotlin/org/neo4j/graphql/examples/dgsspringboot/datafetcher/AdditionalDataFetcher.kt
@@ -3,24 +3,25 @@ package org.neo4j.graphql.examples.dgsspringboot.datafetcher
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
 import graphql.schema.DataFetchingEnvironment
+import org.neo4j.graphql.examples.dgsspringboot.types.DgsConstants
 import org.neo4j.graphql.examples.dgsspringboot.types.types.JavaData
 import java.util.*
 
 
 @DgsComponent
 class AdditionalDataFetcher {
-    @DgsData(parentType = "Movie", field = "bar")
+    @DgsData(parentType = DgsConstants.MOVIE.TYPE_NAME, field = DgsConstants.MOVIE.Bar)
     fun bar(): String {
         return "foo"
     }
 
-    @DgsData(parentType = "Movie", field = "javaData")
+    @DgsData(parentType = DgsConstants.MOVIE.TYPE_NAME, field = DgsConstants.MOVIE.JavaData)
     fun javaData(env: DataFetchingEnvironment): List<JavaData> {
         val title = env.getSource<Map<String, *>>()["title"]
         return Collections.singletonList(JavaData("test $title"))
     }
 
-    @DgsData(parentType = "Query", field = "other")
+    @DgsData(parentType = DgsConstants.QUERY.TYPE_NAME, field = DgsConstants.QUERY.Other)
     fun other(): String {
         return "other"
     }
